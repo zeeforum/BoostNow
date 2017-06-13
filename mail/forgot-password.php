80f8b370ff18e19b8a3769a312e0dd4e27e5f85a
Zartash Zulfiqar <zartashzulfiqar@gmail.com>
2017-06-13 22:50:06 +0500

    Change Reset Password Url

--- mail/forgot-password.php
+++ mail/forgot-password.php
@@ -1,7 +1,7 @@
 <?php
 /* @var $this yii\web\View */
 /* @var $user common\models\User */
-$resetLink = Yii::$app->urlManager->createUrl([Yii::$app->params['adminUrl'] . 'main/reset-password', 'token' => $user->password_reset_token]);
+$resetLink = Yii::$app->params['adminUrl'] . 'main/reset-password?token' . $user->password_reset_token;
 ?>
 Hello <?= $user->username ?>,
 
