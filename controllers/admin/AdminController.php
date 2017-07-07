d65380633baad1a1acda1646374c5b96be2b9912
Zartash Zulfiqar <zartashzulfiqar@gmail.com>
2017-07-07 22:07:14 +0500

    Admin Configurations

--- controllers/admin/AdminController.php
+++ controllers/admin/AdminController.php
@@ -8,6 +8,8 @@
 	class AdminController extends Controller {
 
 		public $layout = '@app/views/admin/layout/main.php';
+		private $admin_role = NULL, $admin = NULL;
+		private $admin_id = 0;
 		/*private $access = [
 			'admin' => [
 				'createCategory' => true,
@@ -36,9 +38,12 @@
 			Yii::$app->params['username'] = $admin_row->identity->username;
 			Yii::$app->params['description'] = $admin_row->identity->description;
 			Yii::$app->params['adminEmail'] = $admin_row->identity->email;
+			$this->admin_role = $admin_row->identity->role;
+			$this->admin_id = $admin_row->identity->id;
 			$formatter = Yii::$app->formatter;
-			Yii::$app->params['date'] =$formatter->asDate($admin_row->identity->created_at, 'long');
+			Yii::$app->params['date'] = $formatter->asDate($admin_row->identity->created_at, 'long');
 			Yii::$app->params['profilePicture'] = $admin_row->identity->picture;
+			$this->admin = Yii::$app->params['adminAbsUrl'];
 			return true;
 		}
 
