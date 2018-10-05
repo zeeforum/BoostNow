9e44acb01d7cabc9d417cdd1d0b7ec759e6b9264
zeeforum <zartashzulfiqar@gmail.com>
2018-10-05 19:33:24 +0500

    Changes in Table Structure - addForeignKey and createIndex

--- migrations/m170618_083616_categories.php
+++ migrations/m170618_083616_categories.php
@@ -5,35 +5,27 @@ use yii\db\Migration;
 
 class m170618_083616_categories extends Migration
 {
-    public function up()
-    {
-        $this->createTable('categories', [
-            'id' => $this->primaryKey(),
-            'name' => $this->string(100)->notNull()->unique(),
-            'description' => $this->string(255),
-            'keywords' => $this->string(255),
-            'detail' => $this->text(),
-            'parent_id' => $this->integer()->notNull()->defaultValue(0),
-            'draft' => "ENUM('no', 'yes') NOT NULL DEFAULT 'no'",
-            'created_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
-            'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
-            'created_by' => $this->integer()->notNull()->defaultValue(0),
-        ]);
-    }
+	public function up()
+	{
+		$this->createTable('categories', [
+			'id' => $this->primaryKey(),
+			'name' => $this->string(100)->notNull()->unique(),
+			'description' => $this->string(255),
+			'keywords' => $this->string(255),
+			'detail' => $this->text(),
+			'parent_id' => $this->integer()->notNull()->defaultValue(0),
+			'draft' => "ENUM('no', 'yes') NOT NULL DEFAULT 'no'",
+			'created_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
+			'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
+			'created_by' => $this->integer()->notNull()->defaultValue(0),
+		]);
 
-    public function down()
-    {
-        $this->dropTable('categories');
-    }
+		$this->addForeignKey('categories_admin_id', 'categories', 'created_by', 'admin', 'id', 'CASCADE');
+	}
 
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
-    }
+	public function down()
+	{
+		$this->dropTable('categories');
+	}
 
-    public function safeDown()
-    {
-    }
-    */
 }
---
+++ migrations/m181005_124221_products.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+/**
+ * Class m181005_124221_products
+ */
+class m181005_124221_products extends Migration
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function up()
+	{
+		$this->createTable('products', [
+			'id' => $this->primaryKey(),
+			'name' => $this->string(255)->notNull(),
+			'title' => $this->string(255)->notNull(),
+			'description' => $this->string(255),
+			'keywords' => $this->string(255),
+			'detail' => $this->text(),
+			'json' => $this->text(),
+			'quantity' => $this->integer()->notNull()->defaultValue(0),
+			'category_id' => $this->integer()->notNull()->defaultValue(0),
+			'draft' => "ENUM('no', 'yes') NOT NULL DEFAULT 'no'",
+			'created_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
+			'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
+			'created_by' => $this->integer()->notNull()->defaultValue(0),
+		]);
+
+		//index
+		$this->createIndex('name', 'products', 'name');
+		$this->createIndex('title', 'products', 'title');
+		$this->createIndex('category_id', 'products', 'category_id');
+
+		//foreign keys
+		$this->addForeignKey('products_category_id', 'products', 'category_id', 'categories', 'id', 'CASCADE');
+		$this->addForeignKey('products_admin_id', 'products', 'created_by', 'admin', 'id', 'CASCADE');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function down()
+	{
+		$this->dropTable('products');
+	}
+
+}
