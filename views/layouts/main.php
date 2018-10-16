a59a5fc2f1660db4176addba52067e69be144af6
zeeforum <zartashzulfiqar@gmail.com>
2018-10-16 17:26:34 +0500

    Fix Header

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,6 +29,7 @@ AppAsset::register($this);
         Thank you for visiting our page.
     </div>
 
+    <!-- top-navigation -->
 	<div class="header-nav nav">
         <div class="container">
             <ul class="nav navbar-nav navbar-right m0">
@@ -42,7 +43,7 @@ AppAsset::register($this);
                     <a href="#">customer care</a>
                 </li>
                 <li>
-                    <a href="#">track my order</a>
+                    <a href="#">terms and conditions</a>
                 </li>
                 <li>
                     <a href="#">login</a>
@@ -52,32 +53,75 @@ AppAsset::register($this);
                 </li>
             </ul>
         </div>
-	</div>
+    </div>
+    <!-- /top-navigation -->
 
+    <!-- header -->
 	<div class="container mt10">
         <div class="main-nav">    
             <div class="navbar">
-                <a class="navbar-brand f36 mt5" href="#">
-                    <span class="light-blue">Boost</span>
-					<span class="light-pink">Now<span>
-                </a>
-                <ul class="search-nav nav navbar-nav navbar-right">
-                    <li class="form">
-                        <form class="navbar-form pull-right">
-                            <input type="text" class="form-control">
-                            <button type="button" class="btn btn-default">Submit</button>
-                        </form>
-                    </li>
-                    <li>
-                        <span class="">
-                            <i class="mt15 fa fa-shopping-cart"></i>
-                        </span>
-                    </li>
-                </ul>
+                <div class="col-xs-12 col-sm-4">
+                    <a class="navbar-brand f36 mt5" href="#">
+                        <span class="light-blue">Boost</span>
+                        <span class="light-pink">Now<span>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-8">
+                    <ul class="search-nav nav navbar-nav navbar-right">
+                        <li class="form">
+                            <form class="navbar-form pull-right">
+                                <input type="text" class="form-control">
+                                <button type="button" class="btn btn-default">Submit</button>
+                            </form>
+                        </li>
+                        <li class="cart-icon">
+                            <a href="#">
+                                <span class="">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
+    <!-- /header -->
     
+    <!-- header-navigation -->
+    <div class="header-main-nav nav">
+        <div class="container">
+            <ul class="nav navbar-nav m0 categories">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Categories
+                        <span class="fa fa-angle-down"></span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-categories">
+                        <li><a href="#">Electronic Devices</a></li>
+                        <li><a href="#">Health & Beauty</a></li>
+                        <li><a href="#">Babies & Toys</a></li>
+                        <li><a href="#">Home & LifeStyle</a></li>
+                        <li><a href="#">Women's Fashion</a></li>
+                        <li><a href="#">Men's Fashion</a></li>
+                        <li><a href="#">Sports & Outdoor</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right m0">
+                <li>
+                    <a href="#">Global Collection</a>
+                </li>
+                <li>
+                    <a href="#">Coupon Code</a>
+                </li>
+                <li>
+                    <a href="#">track my order</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- /header-navigation -->
 
     <div class="container">
         <?= Breadcrumbs::widget([
